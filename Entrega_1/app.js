0330ede9a8620639762600a596ae29fe5e530147
Camilo Polania <cpolania@outlook.com>
2021-09-30 20:08:09 -0500

    Actualizacion #1

--- Entrega_1/app.js
+++ Entrega_1/app.js
@@ -1,18 +1,21 @@
 let nombre = prompt("Ingresa tu nombre")
 let apellido = prompt("Ingresa tu apellido")
-console.log('Bienvenido '+ nombre + " " + apellido);
-console.log('Vamos a realizar operaciones Matematicas básicas');
+alert('Bienvenido '+ nombre + " " + apellido);
+alert('Vamos a realizar operaciones Matematicas básicas');
 let numero1 = parseInt(prompt("Ingrese un número"))
+alert('El número ingresado es: ' + numero1);
 let numero2 = parseInt(prompt("Ingrese otro número"))
+alert('El número ingresado es: ' + numero2);
+
 
 let suma = numero1 + numero2
 let resta = numero1 - numero2
 let multiplicacion = numero1 * numero2
 let division = numero1 / numero2
 
-console.log('La suma de' + " " + numero1 + " "+ 'y' + " " + numero2 + " " + "es: " + suma);
-console.log('La resta de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: "+resta);
-console.log('La multiplicación de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + multiplicacion);
-console.log('La división de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + division);
+alert('La suma de' + " " + numero1 + " "+ 'y' + " " + numero2 + " " + "es: " + suma);
+alert('La resta de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: "+resta);
+alert('La multiplicación de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + multiplicacion);
+alert('La división de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + division);
 
 
