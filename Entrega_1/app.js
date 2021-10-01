afb380937a3f879741938258332f3d3a45faf8b5
Camilo Polania <cpolania@outlook.com>
2021-09-30 20:03:06 -0500

    Ejercicio finalizado

---
+++ Entrega_1/app.js
@@ -0,0 +1,18 @@
+let nombre = prompt("Ingresa tu nombre")
+let apellido = prompt("Ingresa tu apellido")
+console.log('Bienvenido '+ nombre + " " + apellido);
+console.log('Vamos a realizar operaciones Matematicas básicas');
+let numero1 = parseInt(prompt("Ingrese un número"))
+let numero2 = parseInt(prompt("Ingrese otro número"))
+
+let suma = numero1 + numero2
+let resta = numero1 - numero2
+let multiplicacion = numero1 * numero2
+let division = numero1 / numero2
+
+console.log('La suma de' + " " + numero1 + " "+ 'y' + " " + numero2 + " " + "es: " + suma);
+console.log('La resta de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: "+resta);
+console.log('La multiplicación de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + multiplicacion);
+console.log('La división de' + " " + numero1 + " " + 'y' + " " + numero2 + " " + "es: " + division);
+
+
