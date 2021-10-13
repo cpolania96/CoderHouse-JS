4cbc839aed2b022ec9ad225a48fd4acfe26d062f
Camilo Polania <cpolania@outlook.com>
2021-10-12 19:48:35 -0500

    Finaliza ejercicio 3

---
+++ Entrega_3/ejercicio3Polania.js
@@ -0,0 +1,8 @@
+//Ejercicio hallar multiplos
+
+let numero = parseInt(prompt("Hasta que numero deseas contar?"))
+let contador = 0
+while(contador < numero) {
+    contador++
+    alert(contador)
+}
